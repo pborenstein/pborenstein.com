ca921da000e1b611d70f6d96acbb40eb7e4a25fd
Philip Borenstein <pborenstein@gmail.com>
2020-12-11 20:08:46 -0500

    update sync utility

--- utils/sync.sh
+++ utils/sync.sh
@@ -8,10 +8,14 @@ echo dest $dest
 
 rsync -av --exclude=".git/" \
           --exclude=".*" \
+          --exclude="package.json" \
+          --exclude="package-lock.json" \
           --exclude="node_modules/" \
           --exclude="dist/" \
           --exclude="src/posts" \
           --exclude="README.md" \
+          --exclude="src/assets/css/colors.css" \
+          --exclude="src/assets/css/swatches.md" \
           --exclude="src/assets/css/local.css" \
           --exclude="src/_data/config.json" \
           --exclude="src/_data/git.js" \
